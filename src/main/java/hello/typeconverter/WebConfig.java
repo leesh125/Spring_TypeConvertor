4dec371fae07672f7b4b163668faf9875c95c834
leesh125 <chelsea12345@naver.com>
2022-03-12 02:35:27 +0900

    포맷터 적용하기 - WebConfig, registry에 포멧터 등록 후 사용

--- src/main/java/hello/typeconverter/WebConfig.java
+++ src/main/java/hello/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import hello.typeconverter.convertor.IntegerToStringConverter;
 import hello.typeconverter.convertor.IpPortToStringConverter;
 import hello.typeconverter.convertor.StringToIntegerConverter;
 import hello.typeconverter.convertor.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,9 +14,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
+        // 주석처리(우선순위 때문)
         registry.addConverter(new StringToIpPortConverter());
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        // 추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
